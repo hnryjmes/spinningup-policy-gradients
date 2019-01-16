1f267ca8f0b30166baff486bb2314bc5b1ab179d
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-16 14:57:36 +0000

    Add mlp function

--- cartpole-pg.py
+++ cartpole-pg.py
@@ -2,3 +2,9 @@ import tensorflow as tf
 import numpy as np
 import gym
 from gym.spaces import Discrete, Box
+
+def mlp(x, sizes, activation=tf.tanh, output_activation=None):
+  # Build a feedforward neural network.
+  for size in sizes[:-1]:
+    x = tf.layers.dense(x, units=size, activation=activation)
+  return tf.layers.dense(x, units=sizes[-1], activation=output_activation)
